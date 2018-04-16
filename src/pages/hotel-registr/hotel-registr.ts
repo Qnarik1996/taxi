eeac9900e8282a81fdfcc880f05ab0a9d16c6317
Qnarik1996 <qnarik.gyurjoghlyan@mail.ru>
2018-04-16 13:36:30 +0400

    hotel-registr

--- src/pages/hotel-registr/hotel-registr.ts
+++ src/pages/hotel-registr/hotel-registr.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Local } from '../../services/local';
+import { ViewController } from 'ionic-angular/navigation/view-controller';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-hotel-registr',
   templateUrl: 'hotel-registr.html',
 })
 export class HotelRegistrPage {
+  hotelName;
+  name;
+  address;
+  firstName;
+  lastName;
+  phone;
+  email;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  hotel;
+  constructor(public navCtrl: NavController,
+              public formBuilder:FormBuilder,
+              public navParams: NavParams,
+              public local:Local,
+              public viewCtrl:ViewController
+            ) {
+                this.hotelName=this.navParams.get('hotelName');
+                this.hotel = this.formBuilder.group({
+                  hotelName:['',Validators.required],
+                  address:['',Validators.required],
+                  name:['',Validators.required],
+                  firstName:['',Validators.required],
+                  lastName:['',Validators.required],
+                  email:['',Validators.compose([Validators.required,Validators.email])],
+                  phone:['',Validators.required]
+
+    })
+  }
+  close(){
+    this.viewCtrl.dismiss()
+  }
+  save(){
+
+    this.viewCtrl.dismiss()
   }
 
-
 }
+//Validators.pattern('[(][+][0-9]{3}[)][0-9]{2}[ ][0-9]{2}[ ][0-9]{2}[ ][0-9]{2}')
--- src/pages/hotel/hotel.ts
+++ src/pages/hotel/hotel.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ModalController } from 'ionic-angular/components/modal/modal-controller';
 import { HotelRegistrPage } from '../barrel';
-const cards=[  
-  {name:"Vannesa Donavan", tel:"(+374) 546 218 454",photo:'assets/imgs/girl.png'},
-  {name:"Vannesa Donavan", tel:"(+374) 546 218 454",photo:'assets/imgs/girl.png'},
-  {name:"Vannesa Donavan", tel:"(+374) 546 218 454",photo:'assets/imgs/girl.png'},
-  {name:"Vannesa Donavan", tel:"(+374) 546 218 454",photo:'assets/imgs/girl.png'},
-  {name:"Vannesa Donavan", tel:"(+374) 546 218 454",photo:'assets/imgs/girl.png'},
-  {name:"Vannesa Donavan", tel:"(+374) 546 218 454",photo:'assets/imgs/girl.png'},  
+const cards = [
+  { name: "Holiday Inn Hotel", tel: "(+374) 546 218 454", photo: 'assets/imgs/girl.png' },
+  { name: "Holiday hotel", tel: "(+374) 546 218 454", photo: 'assets/imgs/girl.png' },
+  { name: "Holiday hotel", tel: "(+374) 546 218 454", photo: 'assets/imgs/girl.png' },
+  { name: "Holiday Inn Hotel", tel: "(+374) 546 218 454", photo: 'assets/imgs/girl.png' },
+  { name: "Holiday hotel", tel: "(+374) 546 218 454", photo: 'assets/imgs/girl.png' },
+  { name: "Hotel", tel: "(+374) 546 218 454", photo: 'assets/imgs/girl.png' },
 ]
 
 @Component({
@@ -18,14 +18,16 @@ const cards=[
 
 
 export class HotelPage {
-pageName;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl:ModalController) {
-  this.pageName = this.navParams.get('pageName')
+
+  pageName;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController) {
+    this.pageName = this.navParams.get('pageName')
+    
   }
-  cards= cards
-  openHotel(){
-let hotel=this.modalCtrl.create(HotelRegistrPage);
-hotel.present()
+  cards = cards
+  openHotel(item) {
+    let hotel = this.modalCtrl.create(HotelRegistrPage,{'hotelName':item.name});
+    hotel.present()
   }
 
 }
